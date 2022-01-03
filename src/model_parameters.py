df41678f38da6966c45f5c57440e433de07241f6
Béatrice Désy <beatrice.desy.1@ulaval.ca>
2022-01-03 18:04:04 -0500

    correction and testing

--- src/model_parameters.py
+++ src/model_parameters.py
@@ -8,68 +8,62 @@ Author: Béatrice Désy
 Date : 03/01/2022
 """
 
-from abc import ABC
+from abc import ABC, abstractmethod
 import pickle
 
 class Parameters(ABC):
-	@abstractmethod
-	def set_parameters(self, dictionary):
-		pass
+    @abstractmethod
+    def specify(self, dictionary):
+        pass
 
-	@abstractmethod
-	def create_parameters_dict(self):
-		pass
-
-	def load_parameters(self, path_to_dict):
+    def load_from_file(self, path_to_dict):
         dictionary = pickle.load(path_to_dict)
-        self.set_parameters(dictionary)
+        self.specify(dictionary)
 
-    def save_parameters(self, path_for_dict):
-    	dictionary = self.create_parameters_dict()
-    	pickle.dump(dictionary, path_for_dict)
+    def save_to_file(self, path_for_dict):
+        pickle.dump(vars(self), path_for_dict)
 
 class GlobalParameters(Parameters):
-	def set_parameters(self, dictionary):
-		self.dimension = dictionary.dimension
-		self.N = dictionary.N
-		self.mu = dictionary.mu
-		self.beta = dictionary.beta
-		self.euclidean = dictionary.euclidean
-		self.radius = dictionary.radius
+    def specify(self, dictionary):
+        self.dimension = dictionary['dimension']
+        self.N = dictionary['N']
+        self.mu = dictionary['mu']
+        self.beta = dictionary['beta']
+        self.euclidean = dictionary['euclidean']
+        self.radius = dictionary['radius']
+        self.njitable = (self.dimension, self.N, 
+                         self.mu, self.beta, self.radius, 
+                         self.euclidean)
 
-	def create_parameters_dict(self):
-		parameters_dict = {'dimension': self.dimension,
-						   'N': self.N,
-						   'mu': self.mu,
-						   'beta': self.beta,
-						   'euclidean': self.euclidean,
-						   'radius': self.radius}
-		return parameters_dict
 
 class LocalParameters(Parameters):
-	def set_parameters(self, dictionary):
-		self.nodes = dictionary.nodes
-		self.coordinates = dictionary.coordinates
-		self.kappas = dictionary.kappas
-		self.target_degrees = dictionary.target_degrees
-
-	def create_parameters_dict(self):
-		parameters_dict = {'nodes': self.nodes,
-						   'coordinates': self.coordinates,
-						   'kappas': self.kappas,
-						   'target_degrees': self.target_degrees}
-		return parameters_dict
+    def specify(self, dictionary):
+        self.nodes = dictionary['nodes']
+        self.coordinates = dictionary['coordinates']
+        self.kappas = dictionary['kappas']
+        self.target_degrees = dictionary['target_degrees']
 
 class OptimizatonParameters(Parameters):
-	def set_parameters(self, dictionary):
-		self.tol = dictionary.tol
-		self.max_iterations = dictionary.max_iterations
-		self.perturbation = dictionary.perturbation
-		self.verbose = dictionary.verbose
+    def specify(self, dictionary):
+        self.tol = dictionary['tol']
+        self.max_iterations = dictionary['max_iterations']
+        self.perturbation = dictionary['perturbation']
+        self.verbose = dictionary['verbose']
 
-	def create_parameters_dict(self):
-		parameters_dict = {'tol': self.tol,
-						   'max_iterations': self.max_iterations,
-						   'perturbation': self.perturbation,
-						   'verbose': self.verbose}
-		return parameters_dict
+
+#tests
+if __name__ == '__main__':
+    from numba import njit
+    test_dico = {'dimension': 1, 'N': 1000, 'mu': 0.01,
+                 'beta': 3.5, 'euclidean': False, 'radius': 159.14}
+    gp = GlobalParameters()
+    gp.specify(test_dico)
+
+    @njit
+    def foo(global_params):
+        #just a random function of my params
+        print(global_params)
+        print('all good')
+
+    foo(gp.njitable)
+
