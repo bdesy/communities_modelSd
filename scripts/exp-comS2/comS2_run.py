69474b1614671d782e0f51a440eefce8b8cc69b9
Béatrice Désy <beatrice.desy.1@ulaval.ca>
2022-01-04 21:57:03 -0500

    place communities uniformly on the sphere

---
+++ scripts/exp-comS2/comS2_run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Description : Community structure experiment in the S1 and S1 model
+
+Author: Béatrice Désy
+
+Date : 03/01/2022
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0, '../../src/')
+from hyperbolic_random_graph import *
+from time import time
+from comS2_util import *
+
--- scripts/exp-comS2/comS2_util.py
+++ scripts/exp-comS2/comS2_util.py
@@ -20,6 +20,34 @@ from geometric_functions import *
 
 from time import time
 
+def sample_from_fibonacci_sphere(n):
+    indices = np.arange(0, n, dtype=float) + 0.5
+    phi = np.arccos(1 - 2*indices/n)
+    theta = np.pi * (1 + 5**0.5) * indices
+    return np.column_stack((theta, phi))
+
+def place_modes_coordinates_on_sphere(nb_com, place='uniformly'):
+    if place=='uniformly':
+        coordinates = sample_from_fibonacci_sphere(nb_com)
+    elif place=='randomly':
+        coordinates = sample_uniformly_on_hypersphere(nb_com, dimension=2)
+    return coordinates
+
+def get_equal_communities_sizes(nb_com, N):
+    sizes = [int(N/nb_com) for i in range(nb_com)]
+    sizes[0] += (N - int(np.sum(np.array(sizes))))
+    return sizes
+
+def get_communities_coordinates_uniform(nb_com, N, sigma):
+    sizes = get_equal_communities_sizes(nb_com, N)
+    centers = place_modes_coordinates_on_sphere(nb_com, place='uniformly')
+    sigmas = np.ones(nb_com)*sigma
+    thetas, phis = sample_gaussian_clusters_on_sphere(centers, sigmas, sizes)
+    return np.column_stack((thetas, phis))
+
+##tests
+
+
 from mpl_toolkits.mplot3d import Axes3D
 def plot_points_on_sphere(coordinates):
     # Create a sphere
@@ -53,55 +81,6 @@ def plot_points_on_sphere(coordinates):
     plt.tight_layout()
     plt.show()
 
-
-@njit
-def measure_upper_triangular_sum(a, n):
-	total = 0
-	for i in range(n):
-		for j in range(i+1, n):
-			total += a[i,j]
-	return total
-
-@njit
-def measure_upper_triangular_mean(a, n):
-	total = measure_upper_triangular_sum(a, n)
-	return 2 * total / (n*(n-1))
-
-@njit
-def measure_distance_matrix_nonuniformity(D, n):
-	mean = measure_upper_triangular_mean(D, n)
-	nonuniformity = (D - mean)**2
-	mse = measure_upper_triangular_mean(nonuniformity, n)
-	return np.sqrt(mse)
-
-def get_perturbed_coordinates(coordinates, rng, perturbation=0.2):
-	sign = (rng.integers(2)-0.5)*2
-	move = rng.random(coordinates.shape)*perturbation*sign
-	return coordinates+move
-
-def get_state(coordinates, n):
-	D = build_angular_distance_matrix(n, coordinates, 2, False)
-	rmse = measure_distance_matrix_nonuniformity(D, n)
-	mean_distance = measure_upper_triangular_mean(D, n)
-	return rmse, mean_distance
-
-def relax_coordinates(coordinates, n, rng, tol):
-	rmse, mean_distance = get_state(coordinates, n)
-	while rmse > tol:
-		print(rmse)
-		prop_coordinates = get_perturbed_coordinates(coordinates, rng)
-		prop_rmse, prop_mean_distance = get_state(prop_coordinates, n)
-		if (prop_rmse < rmse) and (prop_mean_distance > mean_distance):
-			coordinates = prop_coordinates
-			rmse = prop_rmse
-	return coordinates
-
-def place_modes_coordinates_on_sphere(nb_com, uniform=True):
-	coordinates = sample_uniformly_on_hypersphere(nb_com, dimension=2)
-	if uniform==True:
-		coordinates = relax_coordinates(coordinates, nb_com, np.random.default_rng(), tol=0.52)
-	return coordinates
-
-for i in range(5):
-	c = place_modes_coordinates_on_sphere(8, uniform=True)
-	plot_points_on_sphere(c)
+for i in range(3):
+    c = get_communities_coordinates_uniform(16, 1000, 0.1)
+    plot_points_on_sphere(c)
