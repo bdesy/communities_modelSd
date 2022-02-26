39b63a5f551a00c13fe68423e4daa263743dba56
Béatrice Désy <beatrice.desy.1@ulaval.ca>
2022-02-26 10:53:49 -0500

    clean and add S3 (but analytical problem)

--- scripts/plots/number_neibors.py
+++ scripts/plots/number_neibors.py
@@ -20,32 +20,18 @@ def number_neighbors_S2(nc, dt):
 def number_neighbors_S1(nc, dt):
 	return (nc/np.pi)*dt
 
-def number_nearest_neighbors_S2_edisks(nc):
-	#return 2*(nc*0.5*(1-np.cos(4/(nc)**(0.5))) - 1)
-	return nc*0.5*(1-np.cos(6/(nc)**(0.5))) - 1
-
 def number_nearest_neighbors_S2_disks(nc):
 	angle = 3*np.arccos(1 - 2./nc)
 	return nc*0.5*(1-np.cos(angle)) - 1
 
-def funk(nn, n):
-	out = 1 - np.cos(3*np.sqrt(8*np.pi/(n*nn)))
-	return (n/2)*out - 1 - nn 
-
-def number_nearest_neighbors_S2_polygons(nc):
-	nn = np.zeros(nc.shape)
-	for i in range(len(nc)):
-		nn_i = fsolve(funk, np.sqrt(nc[i]), args=(nc[i]))
-		nn[i] = nn_i[0]
-	return nn
-	
-
+def number_nearest_neighbors_S3(nc):
+	y = 3*(3*nc/2)**(1./3)
+	out = y - np.sin(y)*np.cos(y)
+	return nc*out/np.pi - 1
 
 nc_list = np.array([5,10,15,20,25])
 cmap = matplotlib.cm.get_cmap('viridis')
 dthetas = np.linspace(0, np.pi, 1000)
-
-number_nearest_neighbors_S2_polygons(nc_list)
 
 fig = plt.figure(figsize=(5,7))
 
@@ -80,11 +66,10 @@ plt.show()
 
 nc = np.arange(50)+1
 plt.figure(figsize=(10,4))
-#plt.plot(nc, (number_nearest_neighbors_S2_edisks(nc)), 'o', c='darkcyan', ms=7, label='with euclidean disks')
-plt.plot(nc, (number_nearest_neighbors_S2_disks(nc)), 'o', c='olivedrab', ms=5, label=r'$n_{nn}$')
-plt.plot(nc, (number_nearest_neighbors_S2_disks(nc)).astype(int), 'o', c='tomato', ms=2, label=r'$\lfloor n_{nn}\rfloor$')
+plt.plot(nc, (number_nearest_neighbors_S2_disks(nc)), 'o', c='olivedrab', ms=5, label=r'$n_{nn}$ in $S^2$')
+plt.plot(nc, (number_nearest_neighbors_S3(nc)), 'o', c='coral', ms=5, label=r'$n_{nn}$ in $S^3$')
+#plt.plot(nc, (number_nearest_neighbors_S2_disks(nc)).astype(int), 'o', c='tomato', ms=2, label=r'$\lfloor n_{nn}\rfloor$')
 
-#plt.plot(nc, (number_nearest_neighbors_S2_polygons(nc)), 'o', c='tomato', ms=3, label='with polygons')
 plt.xlabel(r'$n$')
 plt.grid()
 plt.legend()
