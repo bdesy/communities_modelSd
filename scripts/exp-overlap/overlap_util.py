d267ea5e4cec0d50eb0afee5beb8944d62c0454e
Béatrice Désy <beatrice.desy.1@ulaval.ca>
2022-03-02 11:45:44 -0500

    clear integration of overlapping nodes functions

--- scripts/exp-overlap/overlap_util.py
+++ scripts/exp-overlap/overlap_util.py
@@ -40,22 +40,6 @@ def get_community_block_matrix(SD, sizes):
     assert (np.sum(block_mat)-np.sum(SD.probs)<1e-5), 'sum of probs not equal'
     return block_mat
 
-def normal_distribution_function(x, mu, sigma):
-    arg = (x-mu)/sigma
-    value = np.exp(-0.5*arg**2)/(sigma*np.sqrt(2*np.pi))
-    return value
-
-def integrate_overlap_probability_S1(mu1, mu2, sigma, factor=2., show=False):
-    dt = compute_angular_distance(mu1, mu2, dimension=1, euclidean=False)
-    r = factor*sigma
-    if dt > 2*r:
-        p = 0.
-    else:
-        b_i = dt-r
-        b_f = r
-        args = (0, sigma)
-        p, err = quad(normal_distribution_function, b_i, b_f, args)
-    return p
 
 def get_coordinates(N, D, nc, sigma, output_centers=False):
     if output_centers==False:
@@ -74,51 +58,6 @@ def get_coordinates(N, D, nc, sigma, output_centers=False):
             coordinates, centers = get_communities_coordinates(nc, N, sigma, 
                                 place='uniformly', output_centers=output_centers)
         return coordinates, centers
-
-mu_test = np.array([[0.5], [1.5], [0], [2*np.pi]])
-
-assert (integrate_overlap_probability_S1(mu_test[0], mu_test[0], 0.2, factor=10) - 1.)<1e-5
-assert (integrate_overlap_probability_S1(mu_test[2], mu_test[3], 0.2, factor=10) - 1.)<1e-5
-assert integrate_overlap_probability_S1(mu_test[0], mu_test[1], 0.2)<1e-5
-
-def integrate_overlap_probability_S2(mu1, mu2, sigma, factor=2., show=False):
-    dt = compute_angular_distance(mu1, mu2, dimension=2, euclidean=False)
-    r = factor*sigma
-    if dt>2*r:
-        p=0
-        show=False
-    else:
-        x = np.linspace(-2*r, 2*r, 1000)
-        y = np.linspace(-2*r, 2*r, 1000)
-        xx, yy = np.meshgrid(x,y)
-        disk1 = np.where(xx**2 + yy**2 < r, 1, 0)
-        disk2 = np.where((xx-dt)**2 + yy**2 < r, 1, 0)
-        mask = disk1*disk2
-        argument_exp = (xx**2+yy**2)/sigma
-        pdf = np.exp( - argument_exp/2 )/(2*np.pi*sigma)
-        #probability
-        area = np.diff(y)[0]*np.diff(x)[0]
-        p = np.sum(pdf*mask*area)
-        assert p>0., 'probability should not be zero, mu1={}, mu2={}'.format(mu1, mu2)
-    if show==True:
-        plt.imshow(pdf*mask)
-        plt.title(r'$p={}$'.format(p))
-        plt.colorbar()
-        plt.show()
-    return p
-
-def get_overlap_matrix(centers, sigma, sizes, d, factor, show=False):
-    nc = len(sizes)
-    overlap_mat = np.zeros((nc, nc))
-    mus = centers
-    for i in range(nc):
-        for j in range(i+1, nc):
-            if d==2:
-                p = integrate_overlap_probability_S2(mus[i], mus[j], sigma, factor, show=show)
-            elif d==1:
-                p = integrate_overlap_probability_S1(mus[i], mus[j], sigma, factor, show=show)
-            overlap_mat[i,j] = p*(sizes[i]+sizes[j])
-    return overlap_mat+overlap_mat.T
 
 
 def get_order_theta_within_communities(SD, sizes):
@@ -205,31 +144,3 @@ def get_disparities(weights):
     num = weights**2
     Y = np.sum(num, axis=0)
     return Y/strengths
-
-#extracting backbone stuff
-
-def get_degree_comm_seq(SD, sizes):
-    nc = len(sizes)
-    m = get_community_block_matrix(SD, sizes)
-    norm = np.sum(m)/2
-    m = normalize_block_matrix(m, nc)
-    return np.sum(np.where(m > 1./norm, 1, 0), axis=0)
-
-def integrand_alpha_ij(x, k):
-    return (1-x)**(k-2)
-
-def integrate_alpha_ij(k, p_ij):
-    integral, error = quad(integrand_alpha_ij, 0, p_ij, (k))
-    return 1 - (k-1)*integral
-
-def get_alpha_matrix(weights_mat, degrees, nc):
-    strengths = np.sum(weights_mat, axis=0)
-    alpha = np.zeros((nc, nc))
-    for i in range(nc):
-        for j in range(nc):
-            p_ij = weights_mat[i,j]/strengths[i]
-            k = degrees[i]
-            alpha[i,j] = integrate_alpha_ij(k, p_ij)
-    return alpha
-
-
