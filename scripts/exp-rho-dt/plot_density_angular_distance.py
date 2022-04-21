765cbd456c82ae384933ff6f0f38b4caf6d189be
Béatrice Désy <beatrice.desy.1@ulaval.ca>
2022-04-21 09:59:48 -0400

    article ready

--- scripts/exp-rho-dt/plot_density_angular_distance.py
+++ scripts/exp-rho-dt/plot_density_angular_distance.py
@@ -27,16 +27,14 @@ from scipy.special import hyp2f1
 
 from util import *
 
-#matplotlib.rcParams['text.usetex']= True
-font = {'size'   : 12, 
-    'family': 'serif'}
 
-matplotlib.rc('font', **font)
+matplotlib.rc('text', usetex=True)
+matplotlib.rc('font', size=10)
 
 cmap = matplotlib.cm.get_cmap('viridis')
 colors =[cmap(1./20), cmap(1.1/3), cmap(2./3), cmap(9./10), cmap(1.0)]
 
-limit=False
+limit=True
 
 Dthetas = np.linspace(1e-5, np.pi, 100000)
 kappa_i, kappa_j = 10., 10.
@@ -63,7 +61,14 @@ def get_exact_max_location(eta, beta, D):
     print(res)
     return res[0]
 
-plt.figure(figsize=(5, 4))
+def limite_beta_eta(Dthetas, eta, D):
+    nonzero = D*Dthetas**(D-1) / eta**D
+    out = np.where(Dthetas<eta, nonzero, 0)
+    return out
+
+
+fig = plt.figure(figsize=(3.375, 3))
+ax = fig.add_subplot(111)
 
 for D in range(Dmax, Dmin-1, -1):
     beta = ratio * D
@@ -85,13 +90,13 @@ for D in range(Dmax, Dmin-1, -1):
     
     c = colors[D-1]
 
-    plt.plot(Dthetas, pij*rho/denum, color='white', linewidth=6)
+    plt.plot(Dthetas, pij*rho/denum, color='white', linewidth=5)
     plt.plot(Dthetas, pij*rho/denum, label=r'$D = {}$'.format(D), 
-                color=c, linewidth=3.5)
+                color=c, linewidth=3)
 
-    approx_max = get_approx_max_location(eta, beta, D)
-    print(approx_max, get_exact_max_location(eta, beta, D))
-    plt.axvline(approx_max, color=c)
+    #approx_max = get_approx_max_location(eta, beta, D)
+    #print(approx_max, get_exact_max_location(eta, beta, D))
+    #plt.axvline(approx_max, color=c)
     #plt.plot(Dthetas, pij*rho/other_denum, ':', color=c)
     #print('normalisation verif : ', np.sum((pij*rho/denum)[:-1]*np.diff(Dthetas)))
 
@@ -103,15 +108,16 @@ for D in range(Dmax, Dmin-1, -1):
         denum, error = integrated_connection_prob(kappa_i, kappa_j, D, beta, mu=mu, R=R)
         #plt.plot(Dthetas, pij*rho/denum, '-', color='white', linewidth=3, zorder=0)
         plt.plot(Dthetas, pij*rho/denum, '--', color=c, zorder=0)
-        plt.ylim(0, 32)
+        #plt.plot(Dthetas, limite_beta_eta(Dthetas, eta, D), c='k', linewidth=1)
+        #plt.ylim(0, 32)
     else:
         pass
         #plt.axvline(eta, color=c, alpha=0.8, linestyle='--')
         #plt.ylim(0, 60)
 
 #plt.xticks([0, np.pi/8, np.pi/4],['0', r'$\pi/8$', r'$\pi/4$'])
-plt.xlabel(r'$\Delta\theta$')
-plt.ylabel(r'$\rho(\Delta\theta\ |\ \kappa, \kappa^\prime)$')
+plt.xlabel(r'$\theta$ (rad)')
+plt.ylabel(r'$g(\theta\ |\ \eta)$')
 
 handles, labels = plt.gca().get_legend_handles_labels()
 order = [4,3,2,1,0]
@@ -120,71 +126,12 @@ plt.legend([handles[idx] for idx in order], [labels[idx] for idx in order], loc=
 #plt.title(r'$\kappa={}$, $\kappa^,={}$, $\beta/d={}$'.format(kappa_i, kappa_j, ratio))
 plt.ylim(0,34.)
 
-plt.xlim(0., np.pi/4)
-
+plt.xlim(0., 0.7)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
 plt.tight_layout()
-#plt.savefig('figure_densities_empty', dpi=600)
+plt.savefig('figure_densities_article', dpi=600)
 plt.show()
-
-#probability averaged over kappas
-
-rng = np.random.default_rng()
-average_degree = 10.
-y = 2.5
-N = 1000
-k_0 = (y-2) * average_degree / (y-1)
-
-Dthetas = np.linspace(0.001, np.pi/2, 200)
-prob_Dthetas = np.zeros(Dthetas.shape)
-
-def other_integrand(a, D, R, y, mu, k_0, beta, Dt):
-    out = a**(D*(1-y)-1)
-    out *= np.log(((a*R)**D)/(mu*k_0**2))
-    out /= (1 + (Dt/a)**beta)
-    out /= integrated_connection_prob_alpha(a, beta, D)
-    return out
-
-def prefactor(D, R, y, mu, k_0):
-    out = D * R**(D*(1-y)) * mu**(y-1) * (y-1)**2
-    out *= k_0**(2*(y-1))
-    return out
-
-def integrand_alpha(Dtheta, alpha, beta, D):
-    A = 1 / (1 + (Dtheta/alpha)**beta)
-    B = angular_prob(Dtheta, D)
-    return A*B
-
-def integrated_connection_prob_alpha(alpha, beta, D):
-    args = (alpha, beta, D)
-    I, error = quad(integrand_alpha, 0, np.pi, args=args)
-    return I
-
-ratio = 20.
-'''
-for D in range(5, 0, -1):
-    beta = ratio * D
-    mu = compute_default_mu(D, beta, average_degree)
-    R = compute_radius(N, D)
-    a_0 = (mu*k_0**2)**(1./D)/R
-    pref = prefactor(D, R, y, mu, k_0)
-
-    for i in tqdm(range(len(Dthetas))):
-        Dt = Dthetas[i]
-        args = (D, R, y, mu, k_0, beta, Dt)
-        integral, error = quad(other_integrand, a_0, np.inf, args=args)
-        integral *= pref
-        integral *= (np.sin(Dt))**(D-1)
-        prob_Dthetas[i] = integral
-    c = cmap((D+1)/11)
-    plt.plot(Dthetas, prob_Dthetas, color=c, label=r'$D={}$'.format(D))
-plt.xlabel(r'$\Delta\theta$')
-plt.ylabel(r'$\rho(\Delta\theta)$')
-plt.title(r'$\gamma={}$, $<\kappa>={}$, $\beta/D={}$'.format(y, average_degree, ratio))
-plt.legend()
-plt.show()
-
-
-'''
 
 
 
