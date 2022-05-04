d342f9373ee3aa06ac101816102a62fa98b45ef6
Béatrice Désy <beatrice.desy.1@ulaval.ca>
2022-05-04 10:51:57 -0400

    some validation/comparison

---
+++ scripts/exp-overlap/test_other_binarizations.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Description : Test various binarization for computing community degree
+
+Author: Béatrice Désy
+
+Date : 03/05/2022
+"""
+
+
+import numpy as np
+import json
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+
+matplotlib.rc('text', usetex=True)
+matplotlib.rc('font', size=12)
+
+#import sys
+#sys.path.insert(0, '../../src/')
+#from hyperbolic_random_graph import *
+#from hrg_functions import *
+#from geometric_functions import *
+#from overlap_util import *
+
+def get_dict_key(D, dd, nc, beta, f):
+    return 'S{}-'.format(D)+dd+'-{}coms-{}beta-{:.2f}sigmam'.format(nc, beta, f)
+
+def retrieve_data(data_dict, D, dd, nc, beta, frac_sigma_axis, bins):
+    y = []
+    for f in frac_sigma_axis:
+        key = get_dict_key(D, dd, nc, beta, f)+'-degrees-'+bins
+        data = np.array(data_dict[key])
+        y.append(data)
+    y_data = np.array(y)
+    if False:#bins=='backbone':
+        y = np.mean(np.max(y_data, axis=2), axis=1)
+        err = np.std(np.max(y_data, axis=2), axis=1)
+    else:
+        y = np.mean(np.mean(y_data, axis=2), axis=1)
+        err = np.std(np.mean(y_data, axis=2), axis=1)
+    return y, err
+
+def measure_community_degrees_gthreshold(matrices_list, t):
+    data = []
+    for m in matrices_list:
+        m = np.array(m)
+        binary_mat = np.where(m>t, 1, 0)
+        degrees = list(np.sum(binary_mat, axis=0).astype(float))
+        data.append(degrees)
+    return data
+
+def measure_community_degrees_proportional(matrices_list):
+    pass
+
+def measure_community_degrees_backbone(matrices_list, alpha, show=False):
+    data = []
+    for m in matrices_list:
+        mat = 2*np.array(m)/np.sum(m)
+        binary_mat = binarize_using_backbone_method(mat, alpha)
+        degrees = list(np.sum(binary_mat, axis=0).astype(float))
+        data.append(degrees)
+    if show:
+        plt.imshow(binary_mat)
+        plt.colorbar
+        plt.show()
+    return data
+
+def binarize_using_backbone_method(m, alpha):
+    n = m.shape[0]
+    binary_mat = np.zeros(m.shape)
+    for i in range(n):
+        for j in range(i):
+            p_ij = m[i,j] / np.sum(m[i, :])
+            binary_mat[i,j] = int((1-p_ij)**(n-2) < alpha)
+    out = binary_mat + binary_mat.T
+    assert np.max(out)<1.1, 'max is greater than 1'
+    return out
+
+dd='exp'
+frac_sigma_axis = np.linspace(0.05, 0.95, 20)
+beta_r = 3.5
+nc_list = [5,15,25]
+
+t = 10
+
+compute = True
+if compute:
+    with open('data/experiment_entropy_blockmatrices.json', 'r') as read_file:
+        matrices_dict = json.load(read_file)
+    with open('data/experiment_entropy.json', 'r') as read_file:
+        initial_data_dict = json.load(read_file)
+    
+    res = {}
+    for D in [1,2]:
+        beta = beta_r*D
+        for nc in nc_list:
+            for f in frac_sigma_axis:
+                key = get_dict_key(D, dd, nc, beta, f)
+                dist_gthreshold = measure_community_degrees_gthreshold(matrices_dict[key], t)
+                #dist_proportional = measure_community_degrees_proportional(matrices_dict[key])
+                #print(key)
+                dist_backbone = measure_community_degrees_backbone(matrices_dict[key], 0.3, show=False)
+                key+='-degrees'
+                dist_first = initial_data_dict[key]
+                res[key+'-first'] = dist_first
+                res[key+'-gthreshold'+str(int(t))] = dist_gthreshold
+                #res[key+'-proportional'] = dist_proportional
+                res[key+'-backbone'] = dist_backbone
+    
+    #with open('data/community_degrees_various_binarizations.json', 'w') as write_file:
+    #    json.dump(res, write_file, indent=4)
+
+else:
+    with open('data/community_degrees_various_binarizations.json', 'r') as read_file:
+        data_dict = json.load(read_file)
+
+cmap = matplotlib.cm.get_cmap('viridis')
+colors = [cmap(0.), cmap(2.2/5)]
+formats = [':', '--', '-']
+bidon = np.linspace(100, 110, 1000)
+
+for bins in ['first', 'gthreshold'+str(int(t)), 'backbone']:
+    plt.figure(figsize=(5.5, 5))
+    for c in range(len(nc_list)):
+        nc = nc_list[c]
+        plt.plot(bidon, np.ones(bidon.shape), 
+                    formats[c], c='k', alpha=0.5,
+                    label=r'$n = {}$'.format(nc))
+        for D in [1,2]:
+            beta = beta_r*D
+            y, err = retrieve_data(res, D, dd, nc, beta, frac_sigma_axis, bins)
+
+            if nc==25:
+                lab = r'$D = {}$'.format(D)
+            else:
+                lab=None
+
+            plt.plot(frac_sigma_axis, y, linestyle=formats[c], color=colors[D-1], 
+                label=lab)
+            plt.fill_between(frac_sigma_axis, y-err, y+err, 
+                        alpha=0.3, color=colors[D-1], linewidth=0.0)
+    plt.title(bins)
+    plt.ylabel(r'$<k>$')
+    plt.xlabel(r'$\sigma$')
+    plt.xlim(0.05,0.95)
+    #plt.ylim(0, 25)
+    plt.legend(ncol=2)
+    plt.tight_layout()
+    plt.show()
